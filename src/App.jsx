b212c7f95612cf375a2586c91ced6723ded1222d
alex <al3xback@gmail.com>
2024-06-10 12:35:50 +0700

    added first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './components/Header/Header.jsx';
+import ResultGrid from './components/ResultsGrid/ResultGrid.jsx';
+import Footer from './components/Footer/Footer.jsx';
+
+function App() {
+	return (
+		<>
+			<Header />
+			<main className='main'>
+				<div className='container'>
+					<ResultGrid />
+				</div>
+			</main>
+			<Footer />
+		</>
+	);
+}
+
+export default App;
